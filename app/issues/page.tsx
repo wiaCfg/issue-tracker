cc177af47a973e85f977873d42356e5f975086ff
Your Name <you@example.com>
2023-11-07 21:47:25 +0200

    Refactor: organize imports

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { Table } from '@radix-ui/themes';
-import Link from '../components/Link';
+import { IssueStatusBadge, Link } from '@/app/components';
 import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
+import { Table } from '@radix-ui/themes';
 import IssuesAction from './IssuesAction';
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssuesAction />
