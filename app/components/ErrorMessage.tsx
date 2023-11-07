c34636e41111c9d155ad70c2ff12394b7400b9c1
Your Name <you@example.com>
2023-11-07 13:34:57 +0200

    Extract the error message controller

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import { Text } from '@radix-ui/themes';
+import React, { PropsWithChildren } from 'react';
+
+const ErrorMessage = ({ children }: PropsWithChildren) => {
+  if (!children) return null;
+
+  return (
+    <Text as="p" color="red">
+      {children}
+    </Text>
+  );
+};
+
+export default ErrorMessage;
